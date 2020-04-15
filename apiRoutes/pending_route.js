9a13d02f6496c8a856f47c035ccc79767364ec67
HSR <hectorsilvarobles@gmail.com>
2020-04-15 13:06:51 -0700

    moving server code to this app

---
+++ apiRoutes/pending_route.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const {Pending} = require('../models/pendingSchema')
+
+
+// Send requests to pending list
+router.post('/send-request', (req, res) => {
+    const newRequest = new Pending(req.body)
+    newRequest.save((err, doc) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                message: 'Error saving request in pending list',
+                error: err
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            newRequest: newRequest
+        })
+    })
+})
+
+
+// Get all request in pending list
+router.get('/all-requests', (req, res) => {
+    Pending.find()
+    .then(requests => res.status(200).json({
+        success: true,
+        all_pending_requests: requests
+    }))
+})
+
+
+// Remove requests after the admin approves or rejects
+router.delete('/remove/:request_id', (req, res) => {
+    const param = req.params.request_id
+
+    Pending.findById(param)
+    .then(request => request.remove()
+        .then(() => res.status(200).json({
+            success: true,
+            message: 'Successfully removed request from pending list',
+            removedRequest: request
+        })
+        ).catch(err => res.status(400).json({
+            success: false,
+            error: err,
+            message: 'failed to remove request from pending list'
+        }))
+    ).catch(err => res.status(400).json({
+        success: false,
+        error: err
+    }))
+})
+
+
+module.exports = router
---
+++ models/athleteSchema.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+
+const coachPostsSchema = mongoose.Schema({
+    coach_writer: {
+        type: String
+    },
+    coach_id : {
+        type: String
+    },
+    coach_profile_pic: {
+        type: String
+    }, 
+    date_of_post : {
+        type: String
+    },
+    coach_message: {
+        type: String,
+        maxlength: 280,
+        minglength: 10
+    },
+    type_of_post : {
+        type: String
+    }
+})
+
+
+const highlightSchema = mongoose.Schema({
+    coach_writer: {
+        type: String
+    },
+    coach_id: {
+        type: String
+    },
+    video_link : {
+        type: String
+    },
+
+    video_thumbnail : {
+        type: String
+    },
+    date_of_post : {
+        type : String
+    }
+})
+
+
+const performanceLog = mongoose.Schema({
+    coach_writer: {
+        type: String
+    },
+    date_of_post : {
+        type: String
+    },
+    leadership_rating : {
+        type: Number
+    },
+    focus_rating : {
+        type: Number
+    },
+    energy_rating: {
+        type: Number
+    }
+})
+
+
+const athleteSchema = mongoose.Schema({
+    athlete_pic : {
+        type: String
+    },
+    lastname: {
+        type: String,
+        maxlength: 70
+    },
+    firstname: {
+        type: String,
+        maxlength:70
+    },
+    age : {
+        type: Number
+    },
+    coach_posts: [coachPostsSchema],
+    performance_logs : [performanceLog],
+    highlights: [highlightSchema]
+})
+
+
+const Athletes = mongoose.model('Athlete', athleteSchema)
+
+module.exports = {Athletes}
