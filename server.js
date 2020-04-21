d6e3cdf4010215c13256771fd24e172696ebd34d
HSR <hectorsilvarobles@gmail.com>
2020-04-20 22:15:11 -0700

    kim jung

--- server.js
+++ server.js
@@ -12,19 +12,12 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //enables cors
-app.use(cors({
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'origin': '*',
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
-  }));
-
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 
 // Connecting Mongoose Database
