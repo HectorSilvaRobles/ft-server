1c87cb02ef80cfdbdb7b1c55f9dce44204ceffd4
HSR <hectorsilvarobles@gmail.com>
2020-05-19 11:13:13 -0700

    ios needs to work

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(cookieParser());
 //enables cors
 // app.use(cors())
 // app.use(cors({credentials: true, origin: 'http://127.0.0.1:3000'}));
-app.use(cors({credentials: true, origin: 'https://www.abfutboltraining.com/' }));
+app.use(cors({credentials: true, origin: 'https://www.abfutboltraining.com' }));
 
 app.use(function(req, res, next) {
         // res.header("Access-Control-Allow-Origin","http://127.0.0.1:3000");
-        res.header("Access-Control-Allow-Origin","https://www.abfutboltraining.com/");
+        res.header("Access-Control-Allow-Origin", "https://www.abfutboltraining.com/","https://www.abfutboltraining.com");
         res.header('Access-Control-Allow-Headers: Accept, Content-Type, X-Requested-With, x-api-key')
         // res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
